5f81682c52ea271179b83bc15d06f1d29dc8a7d4
unknown <23371323@studentmail.ul.ie>
2023-10-04 11:38:05 +0100

    Create addition function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
     }
+    public static int Addition(int num1, int num2) {
+    	int result = 0; 
+    	result = num1 + num2;
+    	return result;
+    }
+
 }
