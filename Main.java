162d4e8d4b7611802d7543d51c202aa5a3e410ec
unknown <23371323@studentmail.ul.ie>
2023-10-04 11:59:17 +0100

    patch

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        
+        while (true) {
+        System.out.print("Enter 2 numbers");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();}
+        s
+    }
+
+    public static int add(int num1, int num2) {
+    	int result = 0; 
+    	result = num1 + num2;
+    	return result;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
